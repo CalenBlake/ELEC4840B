8570c9e4120801b59a5dddf85c8d07ac71a94403
CalenBlake <CalenB8@gmail.com>
2023-04-01 17:06:01 +1100

    Plotted randomly sampled batch of input data.

--- CyTex_Model_T1.py
+++ CyTex_Model_T1.py
@@ -22,35 +22,49 @@ import torch.utils.data as data
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 # import time
+import random
 import os
 
 # --------------------- 1. Load & Preprocess Data ---------------------
 # NOTE: Will need to sort data into training and test sets. Check how this was done in ENGG3300.
 # This can either be done in code or can make train and val folders in the data directory.
-# a.) load data, train and test
+# a.) load data
 data_dir = './EMODB Database/RGB_IMG/'
 batch_size = 32
 img_height = 400
 img_width = 400
 
-# convert images to tensors while loading
-base_tran_img = transforms.Compose([
-    transforms.ToTensor(),
-])
 # import full dataset -> load from folders
-dataset = datasets.ImageFolder(data_dir, transform=base_tran_img)
+dataset = datasets.ImageFolder(data_dir)
+
+# b.) plot sample of data
+# Get a random subset of images from the dataset
+indices = random.sample(range(len(dataset)), 6)
+subset = [dataset[i] for i in indices]
+# Plot to separate window and remove Matplotlib version 3.6 warnings
+matplotlib.use("TkAgg")
+# Plot the images with classes as titles
+fig, axes = plt.subplots(2, 3, figsize=(12, 6))
+plt.title('Random Sample of Input Data')
+for i, ax in enumerate(axes.flat):
+    ax.imshow(subset[i][0])
+    ax.set_title(dataset.classes[subset[i][1]])
+    # ax.axis('off')
+plt.suptitle('Random Sample of Input Data')
+plt.tight_layout()
+plt.show()
+
+
+
+# c.) separate into training and test sets and apply transforms
 # split data into training and test set, 80/20 split
-train_data, test_data = train_test_split(dataset, test_size=0.2, random_state=42)
+# train_data, test_data = train_test_split(dataset, test_size=0.2, random_state=42)
 # reload data using dataloader objects -> apply transforms to train and test sets
-
-
-# b.) plot sample of train data
-
-
-# c.) transform training data
 
 
 # d.) plot sample of transformed training data
