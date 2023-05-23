8badfa4caef2a36831f6f523e93aeccc39724b20
CalenBlake <CalenB8@gmail.com>
2023-05-23 21:12:00 +1000

    EMODB DCNN update 2 Completed kfold test script.

--- Preprocessing Scripts/kFold_Test.py
+++ Preprocessing Scripts/kFold_Test.py
@@ -20,9 +20,21 @@ import os
 from sklearn.model_selection import StratifiedKFold
 
 # a.) load data %%%%%%%%%%
-data_dir = '../RAVDESS_Refactored/RGB_IMG/'
+data_dir = '../EMODB Database/RGB_IMG/'
 dataset = datasets.ImageFolder(data_dir, transform=transforms.ToTensor())
 
-# b.) separate into k-fold sets %%%%%%%%%%
+# b.) testing subset method %%%%%%%%%%
+x = dataset.imgs    # directory paths to images
+sub_d = data.Subset(dataset, range(int(len(dataset)/2)))
+sub_d2 = data.Subset(dataset, [0, 1, 2])
+
+# c.) create k-fold splits %%%%%%%%%%
+y = dataset.targets     # list of labels
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+splits = skf.split(range(len(y)), y)
+for train_index, test_index in splits:
+    X_train, X_test = [dataset[i][0] for i in train_index], [dataset[i][0] for i in test_index]
+    y_train, y_test = [y[i] for i in train_index], [y[i] for i in test_index]
+
 
 
